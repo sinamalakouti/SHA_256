38bf311192739452415d6ffdb7962620c609fa1e
navidkmn <navid.kmn@gmail.com>
2018-06-05 16:22:52 +0430

    Add CPU and Memory Info, Add VHDL modules

---
+++ VHDL/Adder.vhd
@@ -0,0 +1,11 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Adder is
+  Port (
+  i , clk : in std_logic;
+  o :out std_logic
+         );
+end Adder;
+architecture Behavioral of q5 is 
+end Behavioral;
