926c83985c798e000c39cb3e94d3044bad055e8e
navidkmn <navid.kmn@gmail.com>
2018-06-05 23:51:07 +0430

    implement some procedures

--- VHDL/TypesPackage.vhd
+++ VHDL/TypesPackage.vhd
@@ -2,7 +2,11 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package types is
+    
     type row is std_logic_vector(31 downto 0);
+    type blo is std_logic_vector(511 downto 0);
+    
     type W is array(0 to 63) of row;
     type K is array(0 to 63) of row; 
+    type blocks is array of blo; 
   end package;
--- VHDL/Utils.vhd
+++ VHDL/Utils.vhd
@@ -8,9 +8,9 @@ package Utils is
   procedure padding(signal inp : in std_logic_vector;
                     signal outp : out std_logic_vector);
   procedure parsing(signal inp : in std_logic_vector;
-                    signal outp : out std_logic_vector);
+                    signal outp : out blocks);
   procedure expansion(signal inp : in std_logic_vector);
-  procedure reverse(signal inp : in std_logic_vector;
+  procedure permutation(signal inp : in std_logic_vector;
                     signal outp : out std_logic_vector);
   procedure multiply(signal inp : in std_logic_vector;
                       signal int : in INTEGER;
@@ -28,10 +28,44 @@ package Utils is
   procedure ch(signal x, y, z : in std_logic_vector;
                signal outp : out std_logic_vector);
   procedure maj(signal x, y, z : in std_logic_vector;
-                signal outp : out std_logic_vector);
-                
+                signal outp : out std_logic_vector);                
 end package;
 
 package body Utils is
+  
+  procedure multiply(signal inp : in std_logic_vector;
+                      signal int : in INTEGER;
+                    signal outp : out std_logic_vector) is
+    variable res : std_logic_vector;
+    begin
+      res := inp;
+      multi : for i in 0 to (int - 1) loop
+        res : inp + res;
+      end loop;         
+      outp <= res;           
+  end procedure;
+  
+  procedure permutation(signal inp : in std_logic_vector(31 downto 0);
+                    signal outp : out std_logic_vector(31 downto 0)) is 
+    begin
+      outp(31 downto 24) <= inp (0 to 7);
+      outp(23 downto 16) <= inp (15 downto 8);
+      outp(15 downto 0) <= inp (15 to 31);
+  end procedure;
+  
+  procedure parsing(signal inp : in std_logic_vector;
+                    signal outp : out blocks) is
+    variable numberOfBlocks : INTEGER;
+    variable index : INTEGER;
+    variable temp : std_logic_vector(511 downto 0);
+      begin
+       numberOfBlocks := inp'length / 512;
+       index := 0;
+       padd : for i in 1 to (numberOfBlocks + 1) loop
+         temp := inp( (i * 512) - 1 downto (i - 1) * 512);
+         outp(index) := temp;
+         temp := temp + 1;
+       end loop;
+  end procedure;
   
 end package body;
